cabe1889d6c5bf60c312181e22d8b1e4916b784b
penteleychuk <penteleychuk@gmail.com>
2016-10-03 16:13:59 +0300

    fix last name

---
+++ HomeWork1/src/main/java/FlowerBed/FlowerBed.java
@@ -0,0 +1,33 @@
+package FlowerBed;
+
+import inputMethod.Input;
+
+import java.util.Scanner;
+
+public class FlowerBed {
+
+    private double radius;
+
+    public static void main(String[] args){
+
+        FlowerBed flower = new FlowerBed();
+        flower.allAction();
+    }
+
+    private void allAction(){
+        radius = Input.inputDouble("Enter radius flower bed");
+        output(radius);
+    }
+
+    private void output(double radius){
+        System.out.printf("Flower bed area      = %.4f%n", getArea(radius));
+        System.out.printf("Flower bed perimeter = %.4f%n", getPerimeter(radius));
+    }
+
+    private double getArea(double radius) {
+        return Math.PI*radius;
+    }
+    private double getPerimeter(double radius){
+        return 2*Math.PI*radius;
+    }
+}
---
+++ HomeWork1/src/main/java/User/Users.java
@@ -0,0 +1,36 @@
+package User;
+
+/**
+ * Created by roma on 28.09.2016.
+ */
+public class Users {
+
+    private String name;
+    private Integer age;
+    private String adr;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setAge(Integer age){
+        this.age = age;
+    }
+
+    public Integer getAge(){
+        return age;
+    }
+
+    public void setAdr(String adr){
+        this.adr = adr;
+    }
+
+    public String getAdr() {
+        return adr;
+    }
+
+}

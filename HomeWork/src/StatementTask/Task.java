488f823dacb864da2ecbabbc618ed8cc211692bc
yurapif <igeekua@gmail.com>
2016-10-11 12:47:36 +0300

    Add statement task

---
+++ HomeWork/src/StatementTask/Task.java
@@ -0,0 +1,20 @@
+package StatementTask;
+
+import java.util.Scanner;
+
+/**
+ * Created by evil on 11.10.16.
+ */
+public class Task {
+    public static void main(String[] args) {
+        int [] number = new int[3];
+        Scanner in = new Scanner(System.in);
+        for (int i = 0; i < number.length; i++){
+            System.out.println("Input number");
+            number[i] = in.nextInt();
+        }
+        for (int i = 0; i < number.length; i++){
+            if (number[i] % 2 != 0) System.out.println("Odd number is " + number[i]);
+        }
+    }
+}

df2bd5d91a7371f98ce0fbb9b7ec2110826de871
Maksym Maksymenko <maximenkom@ukr.net>
2016-10-13 15:57:17 +0300

    modified output for animals methods

--- Lesson 5 Practical Task/startinterface/Main.java
+++ Lesson 5 Practical Task/startinterface/Main.java
@@ -1,30 +1,34 @@
 package startinterface;
 
+import java.util.List;
 import java.util.ArrayList;
-
-
+/*Create interface Animal with methods voice() and feed(). 
+ * Create two classes Cat and Dog, which implement this interface.
+ *  Create array of Animal and add some Cats and Dogs to it. 
+ * Call voice() and feed() method for all of it
+ */
 
 public class Main {
 	public static void main(String[] args) {
-		ArrayList<String> animalsNameList = new ArrayList<String>();
+
+		ArrayList<Animal> animalsNameList = new ArrayList<>();
 		Cat cat1 = new Cat("Barsik");
-		animalsNameList.add(cat1.name);
+		animalsNameList.add(cat1);
 		Cat cat2 = new Cat("Pushok");
-		animalsNameList.add(cat2.name);
+		animalsNameList.add(cat2);
 		Cat cat3 = new Cat("Kuzia");
-		animalsNameList.add(cat3.name);
+		animalsNameList.add(cat3);
 		Dog dog1 = new Dog("Bobik");
-		animalsNameList.add(dog1.name);
+		animalsNameList.add(dog1);
 		Dog dog2 = new Dog("Sharik");
-		animalsNameList.add(dog2.name);
+		animalsNameList.add(dog2);
 		Dog dog3 = new Dog("Shrek");
-		animalsNameList.add(dog3.name);
+		animalsNameList.add(dog3);
 
-		/*for (int i = 0; i < animalsNameList.size(); i++) {
-			System.out.println(animalsNameList.get(i));
-		}*/
-		cat1.voice();
-		dog1.feed();
-
+		for (Animal animal : animalsNameList) {
+			animal.feed();
+			animal.voice();
+		}
 	}
 }
+

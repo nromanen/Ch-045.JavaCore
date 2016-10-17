842dea0177c3ba8e7792531b1b7b5777e7ac3e9e
yurapif <igeekua@gmail.com>
2016-10-17 20:12:38 +0300

    try run task with new method

--- HomeWorkOOP/src/AnimalTask/AppMain.java
+++ HomeWorkOOP/src/AnimalTask/AppMain.java
@@ -3,6 +3,13 @@ package AnimalTask;
 /**
  * Created by evil on 17.10.16.
  */
+
+/*
+    Create interface Animal with methods voice() and feed().
+    Create two classes Cat and Dog, which implement this interface.
+    Create array of Animal and add some Cats and Dogs to it.
+    Call voice() and feed() method for all of it
+ */
 public class AppMain {
     public static void main(String[] args) {
         AnimalList animalList = new AnimalList();

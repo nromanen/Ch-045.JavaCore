9d9c73daa096cf820eec20a791b0977aa112669e
Maksym Maksymenko <maximenkom@ukr.net>
2016-10-19 00:25:40 +0300

    updated

--- Lesson 6 Practical Task/lesson6practicaltask/ReturnArrayMoreThan5.java
+++ Lesson 6 Practical Task/lesson6practicaltask/ReturnArrayMoreThan5.java
@@ -6,15 +6,11 @@ public class ReturnArrayMoreThan5 {
 	public static ArrayList<Integer> returnIndexNumbersBiggerThan5(int a, ArrayList<Integer> list) {
 		ArrayList<Integer> newArray = new ArrayList<Integer>();
 		for (int i = 0; i < list.size(); i++) {
-			//if (i == 0 && list.get(i) > a) {
-			//	newArray.add(i);
-			//i++;
-		//	}
-
+		
 			 if (list.get(i) > a) {
-				// System.out.println(list);
+				
 				list.remove(i);
-				// i++;
+				
 				newArray.add(i);
 			}
 			

5065cb750204c4ea392978182839ac7182b53aa3
yurapif <igeekua@gmail.com>
2016-10-19 21:00:37 +0300

    add new completed task

--- HomeWorkOOP/src/Employee/Employee.java
+++ HomeWorkOOP/src/Employee/Employee.java
@@ -1,8 +1,44 @@
 package Employee;
 
-/**
- * Created by evil on 17.10.16.
- */
-public interface Employee {
+import java.util.*;
 
+/**
+ * Created by evil on 18.10.16.
+ */
+public abstract class Employee {
+    private static int countEmployee = 0;
+    private int id;
+    private String name;
+
+    public Employee(){
+        this.id = ++countEmployee;
+    }
+
+    public Employee(String name){
+        this.id = ++countEmployee;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static int getCountEmployee() {
+        return countEmployee;
+    }
+
+    public abstract int getAverageMonthlySalary();
+    public abstract void printInf();
 }

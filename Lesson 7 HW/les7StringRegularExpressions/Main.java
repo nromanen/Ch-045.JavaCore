d22709282b2548245f94d265ae3746ec221b97a3
Maksym Maksymenko <maximenkom@ukr.net>
2016-10-21 01:49:43 +0300

    1st task

---
+++ Lesson 7 HW/les7StringRegularExpressions/Main.java
@@ -0,0 +1,16 @@
+package les7StringRegularExpressions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+	
+	public static void main(String[] args) throws IOException
+	{
+	//	LongestWord lw = new LongestWord();
+	//lw.longestWord();
+	SecondWordInReverseOrder sec = new SecondWordInReverseOrder();
+	 sec.SecondWordInReverseOrder();
+	}
+}

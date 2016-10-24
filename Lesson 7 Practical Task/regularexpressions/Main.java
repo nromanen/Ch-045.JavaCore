dafaef8c48b3981e5a292d6c10be3fc0cb854b87
Maksym Maksymenko <maximenkom@ukr.net>
2016-10-24 10:19:11 +0300

    repaired mistakes

--- Lesson 7 Practical Task/regularexpressions/Main.java
+++ Lesson 7 Practical Task/regularexpressions/Main.java
@@ -1,15 +1,34 @@
 package regularexpressions;
+/*
+ *1. Enter in the console sentence of five words. 
+display the longest word in the sentence
+determine the number of its letters
+bring the second word in reverse order
+2.
+Enter a sentence that contains the words between more than one space. Convert all spaces,
+ consecutive, one. For example, if we introduce the sentence "I    am      learning     Java   Core», 
+ we have to get the "I'm learning Java Core»
+3.Implement a pattern for US currency: the first symbol "$", then any number of digits, dot and two digits after the dot. 
+Enter the text from the console that contains several occurrences of US currency. Display all occurrences on the screen.
+
+ */
+
+
 
 import java.io.IOException;
+
+import les7StringRegularExpressions.RemoveExtraSpaces;
 
 public class Main {
 	public static void main(String[] args) throws IOException
 	{
+		
 		TryWordForMatcher tr = new TryWordForMatcher();
-		System.out.println(tr.TryWord("Maksym-ggA"));
-		System.out.println(tr.IfContains("mother", "I love my mother"));
-		tr.EnterNameAndOutPutName();
-		tr.EnterNameAndOutPutNamefromOneString();
+		System.out.println(tr.isMatches("Maksym-ggA","[A-Z]{1}[a-z]{2,14}(-[A-Z]{1}[a-z]{2,14})?"));
+		System.out.println(tr.ifContains("mother", "I love my mother"));
+	//	tr.enterNameAndOutPutName();
+		tr.enterNameAndOutPutNamefromOneString(); 
+		
 		
 	}
 }
--- Lesson 7 Practical Task/regularexpressions/TryWordForMatcher.java
+++ Lesson 7 Practical Task/regularexpressions/TryWordForMatcher.java
@@ -8,26 +8,28 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class TryWordForMatcher {
-	public boolean TryWord(String text) {
-		String pattern = "[A-Z]{1}[a-z]{2,14}(-[A-Z]{1}[a-z]{2,14})?";
+	public boolean isMatches(String text, String pattern) {
+		 pattern = "[A-Z]{1}[a-z]{2,14}(-[A-Z]{1}[a-z]{2,14})?";
 
 		Pattern p = Pattern.compile(pattern);
 		Matcher m = p.matcher(text);
 		return m.matches();
 	}
 
-	public boolean IfContains(String s1, String s2) {
+	public boolean ifContains(String s1, String s2) {
 		String pattern = s1;
 		Pattern p = Pattern.compile(pattern);
 		Matcher m = p.matcher(s2);
 		return m.find();
 	}
 
-	public void EnterNameAndOutPutName() throws IOException {
-		System.out.println("Input please name, patronomic and lastname: ");
+	public void enterNameAndOutPutName() throws IOException {
+		System.out.println("Input please name : ");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String name = reader.readLine();
+		  System.out.println("Input please patronomic : ");
 		String patronomic = reader.readLine();
+		 System.out.println("Input please lastname : ");
 		String lastName = reader.readLine();
 		char nameFirst = name.charAt(0);
 		char patronomicFirst = patronomic.charAt(0);
@@ -36,7 +38,7 @@ public class TryWordForMatcher {
 
 	}
 
-	public void EnterNameAndOutPutNamefromOneString() throws IOException {
+	public void enterNameAndOutPutNamefromOneString() throws IOException {
 		System.out.println("Input please name, patronomic and lastname in one string(use space): ");
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		String name = reader.readLine();

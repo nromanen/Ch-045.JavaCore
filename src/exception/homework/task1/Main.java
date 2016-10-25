cd35f3ca408d69463b067884c656155e84bd8879
Roma Mocherniuk <roma_ariezz@ukr.net>
2016-10-25 12:29:30 +0300

    exceptions: practice tasks and homework

--- src/exception/homework/task1/Main.java
+++ src/exception/homework/task1/Main.java
@@ -7,21 +7,36 @@ package exception.homework.task1;
 
 import exception.homework.task2.IncorrectIntegerException;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
 import static util.Print.println;
 
 public class Main {
     public static void main(String[] args) {
 
+        Double d1 = 0.0;
+        Double d2 = 0.0;
         try {
-            println(Util.div());
-        } catch (IOException e) {
-            println("IOException: " + e.getMessage());
+            try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                d1 = Double.parseDouble(br.readLine());
+                d2 = Double.parseDouble(br.readLine());
+            } catch (IOException e) {
+                println("IOException: " + e.getMessage());
+                System.exit(-2);
+            }
+
         } catch (NumberFormatException e){
             println("NumberFormatException: " + e.getMessage());
+            System.exit(-3);
+        }
+
+        try {
+            println(Util.div(d1, d2));
         } catch (ArithmeticException e) {
             println("ArithmeticException: " + e.getMessage());
+            System.exit(-4);
         }
     }
 }
--- src/exception/homework/task1/Util.java
+++ src/exception/homework/task1/Util.java
@@ -1,28 +1,15 @@
 package exception.homework.task1;
 
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
 public class Util {
 
     // Method read two double numbers d1 and d2 and return divide d1/d2
-    public static double div() throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        Double d1 = 0.0;
-        Double d2 = 0.0;
-        try {
-            d1 = Double.parseDouble(br.readLine());
-            d2 = Double.parseDouble(br.readLine());
-        } catch (NumberFormatException e){
-            throw new NumberFormatException();
-        }
+    public static double div(double d1, double d2) {
+
         if (d2 == 0){
             throw new ArithmeticException("Divide by zero");
         }
 
-        br.close();
         return d1/d2 ;
     }
 }
